5e3ba53b5579c3af1e48053fc937a0aca25dde65
RCattoi <rodrigoccattoi@gmail.com>
2022-12-02 12:25:16 -0300

    feat: show right content when img is clicked

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const tabMenu = document.querySelectorAll(".js-tabMenu img");
+const tabcontent = document.querySelectorAll(".js-tabContent section");
+
+// function callback(event) {
+//   clickedImg = event.target;
+//   console.log(clickedImg);
+//   tabMenu.forEach((item) => {
+//     item.classList.remove("active");
+//     if (clickedImg === item) {
+//       item.classList.add("active");
+//     }
+//   });
+// }
+
+// addEventListener("click", callback);
+// console.log(tabMenu.indexOf("img.active"));
+
+tabcontent.forEach((item) => {
+  // item.classList.remove("active");
+  item.style.display = "none";
+});
 
 function callback(event) {
   clickedImg = event.target;
-  tabMenu.forEach((item) => {
+  console.log(clickedImg);
+  tabcontent.forEach((item) => {
+    item.classList.remove("active");
+    item.style.display = "none";
+  });
+  tabMenu.forEach((item, position) => {
     item.classList.remove("active");
     if (clickedImg === item) {
-      item.classList.add("active");
+      tabcontent[position].classList.add("active");
+      tabcontent[position].style.display = "block";
     }
   });
 }
