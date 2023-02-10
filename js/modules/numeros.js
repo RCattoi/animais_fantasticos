867fc4af500016456263ebe37414f14d02f42962
RCattoi <rodrigoccattoi@gmail.com>
2023-02-10 01:08:57 -0300

    feat:

--- js/modules/numeros.js
+++ js/modules/numeros.js
@@ -1,16 +1,34 @@
 export default function initAnimaNumeros() {
-  const numeros = document.querySelectorAll(".numeros-animal span");
-  let timer,
-    contador = 0;
+  function aanimaNumeros() {
+    const numeros = document.querySelectorAll(".numeros-animal span");
+    let timer;
+    function animacaoNumero(animalValor, item) {
+      let contador = 0;
+      let incremento = Math.floor(animalValor / 100);
 
-  function initAnimaisNumeros(animalValor, item) {
-    timer = setInterval(() => {
-      item.innerHTML = contador;
-      contador <= animalValor ? (contador += 1) : clearInterval(timer);
+      timer = setInterval(() => {
+        item.innerHTML = contador;
+        contador = contador + incremento;
+        if (contador > animalValor) {
+          item.innerHTML = animalValor;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
+    }
+    numeros.forEach((item) => {
+      const numerosValor = +item.innerHTML;
+      animacaoNumero(numerosValor, item);
     });
   }
-  numeros.forEach((item) => {
-    const numerosValor = +item.innerHTML;
-    initAnimaisNumeros(numerosValor, item);
-  });
+}
+function handleMutation(mutation) {
+  if (mutation[0].target.classList.contains("animar")) {
+    aanimaNumeros();
+    observer.disconnect;
+  }
+
+  const observerTarget = document.querySelector(".numeros");
+  const observer = new MutationObserver(handleMutation);
+
+  observer.observe(observerTarget, { attributes: true });
 }
